c33b3084e615a36e17a8985023276b8bb5241c31
Manabu Morita <m-morita@nri.co.jp>
2021-02-01 21:19:55 +0900

    initial

---
+++ rplugin/python3/calcdate.py
@@ -0,0 +1,23 @@
+import pynvim
+
+from datetime import date, timedelta
+
+@pynvim.plugin
+class CalcDatePlugin(object):
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.command('CalcDate', nargs='*', range='')
+    def calcdate(self, args, range):
+        try:
+            days = int(args[0])
+        except:
+            days = 0
+
+        today = date.today()
+        target = today - timedelta(days=days)
+
+        self.nvim.feedkeys(f"g,f{target}\n")
+
+
